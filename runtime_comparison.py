7bd5febd4673e26260e8f539ecc123637a57c2b3
LostBlueDotInhabitant <albi.cullhaj2022@gmail.com>
2026-03-28 13:17:54 -0400

    Add files via upload

--- runtime_comparison.py
+++ runtime_comparison.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import function
 from qiskit import QuantumCircuit
 import numpy as np
-max_qubit = 20
+max_qubit = 30
 max_time  = 60
-
+runs  =3
 times = []
 solution = [0]
 
 for n in range(2, max_qubit + 1):
     qc = QuantumCircuit(n,n)
     start_time = time.perf_counter()
-    function.full_grover(qc,solution, n)
+    for i in range(runs):
+        function.full_grover(qc,solution, n)
     end_time = time.perf_counter()
     
-    elapsed_time = end_time - start_time
+    elapsed_time = (end_time - start_time)/runs
     times.append(elapsed_time)
     
     print(f"Qubits: {n}, {elapsed_time:.4f} seconds")
@@ -24,10 +25,12 @@ for n in range(2, max_qubit + 1):
         print("Exceeded maximum time limit. Stopping.")
         break
 
-times = np.array(times)
-times = np.sqrt(times)
+
 plt.plot(range(2, max_qubit + 1), times)
 plt.xlabel("Qubits")
 plt.ylabel("Time (seconds)")
 plt.title("Runtime Comparison")
 plt.show()
+
+print(list(range(2, max_qubit + 1)))
+print(times)
